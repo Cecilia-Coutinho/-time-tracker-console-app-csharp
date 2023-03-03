ad57103ab56ecb4e8f6d906333fc7a6dd9b83251
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-03-03 23:09:05 +0100

    UpdatePerson() Version updated

--- PersonData.cs
+++ PersonData.cs
@@ -45,10 +45,11 @@ namespace TimeTrackeConsoleApp
             Program.BannerMessageScreen();
             try
             {
-                string? oldPersonName = ListPersonsForSelection();
+                Console.Write("\n\tEnter username you want update: ");
+                string? oldPersonName = Console.ReadLine()?.ToLower();
                 Console.Write("\n\tEnter the new username: ");
                 string? newPersonName = Console.ReadLine();
-                if (string.IsNullOrEmpty(newPersonName))
+                if (string.IsNullOrEmpty(oldPersonName) || string.IsNullOrEmpty(newPersonName))
                 {
                     Console.WriteLine($"\n\tError: It's not a valid Name.\n");
                     return;
@@ -56,7 +57,7 @@ namespace TimeTrackeConsoleApp
                 else
                 {
                     PostgresDataAccess.UpdatePersonData(oldPersonName, newPersonName.ToLower());
-                    Console.WriteLine($"\tPerson successfully updated: {oldPersonName} is now {newPersonName}.");
+                    Console.WriteLine($"\tPerson successfully updated: {oldPersonName} now is {newPersonName}.");
                 }
             }
             catch (Exception ex)
@@ -69,78 +70,12 @@ namespace TimeTrackeConsoleApp
             }
         }
 
-        static string? ListPersonsForSelection()
-        {
-            List<PersonData>? listPersons = PostgresDataAccess.GetListAllPersons();
-            bool runMenu = true;
-            int menuIndex = 1;
-            string menuName = "Select username to Edit";
-
-            while (runMenu)
-            {
-                Clear();
-                WriteLine($"\t {menuName.ToUpper()}:");
-                int maxIndexToSelect = listPersons.Count;
-                for (int i = 0; i < maxIndexToSelect; i++)
-                {
-                    if (i == menuIndex - 1)
-                    {
-                        ForegroundColor = ConsoleColor.DarkYellow;
-                    }
-                    Write($"\n\t {i + 1}. {listPersons[i].person_name}\n");
-                    ResetColor();
-                }
-
-                ConsoleKeyInfo keyInfo = ReadKey(true);
-
-                if (keyInfo.Key == ConsoleKey.Enter)
-                {
-                    return listPersons[menuIndex - 1].person_name;
-                }
-                else
-                {
-                    int minIndexToSelect = 1;
-
-                    switch (keyInfo.Key)
-                    {
-                        case ConsoleKey.UpArrow:
-                            if (menuIndex > minIndexToSelect)
-                            {
-                                menuIndex--;
-                            }
-                            break;
-                        case ConsoleKey.DownArrow:
-                            if (menuIndex < maxIndexToSelect)
-                            {
-                                menuIndex++;
-                            }
-                            break;
-                    }
-                }
-            }
-            return null;
-        }
-
         public static void DisplayAllPersons()
         {
-            List<PersonData> listPersons = PostgresDataAccess.GetListAllPersons();
-            if (listPersons?.Count > 0)
-            {
-                //Console.WriteLine($"Retrieved {listPersons.Count} users:");
-                string menuName = $"{listPersons.Count} users found";
-                List<MenuSystem> personToSelect = new();
-                foreach (PersonData person in listPersons)
-                {
-                    //Console.WriteLine($"Person Name: {person.person_name}");
-                    personToSelect.Add(new MenuSystem($"{person.person_name}", () => Console.WriteLine($"")));
-                }
-                Program.BannerMessageScreen();
-                MenuSystem.RunMenu(menuName, personToSelect);
-            }
-            else
-            {
-                Console.WriteLine("No persons found");
-            }
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("coming soon");
+            Console.ResetColor();
+
         }
 
         public static void DisplayPersonByProject()
