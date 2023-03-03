76372852175f864d32cc78fc0d05acaa622667a3
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-03-03 17:22:44 +0100

    UpdatePerson() Feature added

--- PersonData.cs
+++ PersonData.cs
@@ -45,9 +45,32 @@ namespace TimeTrackeConsoleApp
 
         public static void UpdatePerson()
         {
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
-            Console.WriteLine("coming soon");
-            Console.ResetColor();
+            Program.BannerMessageScreen();
+            try
+            {
+                Console.Write("\n\tEnter username you want update: ");
+                string? oldPersonName = Console.ReadLine()?.ToLower();
+                Console.Write("\n\tEnter the new username: ");
+                string? newPersonName = Console.ReadLine()?.ToLower();
+                if (string.IsNullOrEmpty(oldPersonName) || string.IsNullOrEmpty(newPersonName))
+                {
+                    Console.WriteLine($"\n\tError: It's not a valid Name.\n");
+                    return;
+                }
+                else
+                {
+                    PostgresDataAccess.UpdatePersonData(oldPersonName, newPersonName);
+                    Console.WriteLine($"\tPerson successfully updated: {oldPersonName} now is {newPersonName}.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\n\tError: The provided name is either already in use\n" +
+                    $"\tor exceeds the maximum length of 25 characters.\n" +
+                    $"\t{ex.Message}");
+                Console.ResetColor();
+            }
         }
 
         public static void DisplayAllPersons()
